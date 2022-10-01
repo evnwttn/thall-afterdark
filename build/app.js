d582b70a8bd6847be688e53415160c165fa1736e
evnwttn <evnwttn@gmail.com>
2022-10-01 14:20:34 -0230

    build

--- build/app.js
+++ build/app.js
@@ -6,10 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 4000;
-app.get("/", (req, res) => {
+app.post("/", (req, res) => {
     res.send("Hello World!");
 });
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`app listening on port ${port}`);
 });
 //# sourceMappingURL=app.js.map
