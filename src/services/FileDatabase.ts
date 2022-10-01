10fe87c87e1772ded16935f79593b9aa522766df
evnwttn <evnwttn@gmail.com>
2022-10-01 15:27:57 -0230

    fs appendfile

--- src/services/FileDatabase.ts
+++ src/services/FileDatabase.ts
@@ -1,8 +1,12 @@
 import { Database } from "./Database";
 import { TimeObject } from "../types";
+import * as fs from "fs/promises";
+import * as os from "os";
 
 export class FileDatabase extends Database {
   async logTime(timeObject: TimeObject): Promise<TimeObject> {
+    await fs.appendFile("timelog.json", JSON.stringify(timeObject) + os.EOL);
+
     return timeObject;
   }
 }
