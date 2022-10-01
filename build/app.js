6a86f51fe279ef6c909328514b715899cbbbb0ec
evnwttn <evnwttn@gmail.com>
2022-10-01 19:19:40 -0230

    initialize app

--- build/app.js
+++ build/app.js
@@ -4,10 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const app_1 = require("firebase/app");
 const middleware_1 = require("./middleware");
 const handlers_1 = require("./handlers");
 const app = (0, express_1.default)();
 const port = 4000;
+const firebaseApp = (0, app_1.initializeApp)({
+    apiKey: "AIzaSyD_LB7UtRh32wQ6mPZv6Wnp8q7EK-HIDYg",
+    authDomain: "thall-30b94.firebaseapp.com",
+    projectId: "thall-30b94",
+    storageBucket: "thall-30b94.appspot.com",
+    messagingSenderId: "549197004508",
+    appId: "1:549197004508:web:e14d810d640d9225e67e19",
+    measurementId: "G-ELKEZCLQVZ",
+});
 app.use(middleware_1.cors);
 app.post("/", handlers_1.postHandler);
 app.listen(port, () => {
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import express from "express";
+import { initializeApp } from "firebase/app";
 import { cors } from "./middleware";
 import { postHandler } from "./handlers";
 
 const app = express();
 const port = 4000;
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyD_LB7UtRh32wQ6mPZv6Wnp8q7EK-HIDYg",
+  authDomain: "thall-30b94.firebaseapp.com",
+  projectId: "thall-30b94",
+  storageBucket: "thall-30b94.appspot.com",
+  messagingSenderId: "549197004508",
+  appId: "1:549197004508:web:e14d810d640d9225e67e19",
+  measurementId: "G-ELKEZCLQVZ",
+});
 
 app.use(cors);
-
 app.post("/", postHandler);
 
 app.listen(port, () => {
