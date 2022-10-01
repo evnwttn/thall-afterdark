8be3e769191983babf481c412bbbc3738dc4b331
evnwttn <evnwttn@gmail.com>
2022-10-01 19:35:53 -0230

    firestore handler

---
+++ build/handlers/firestoreHandler.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("firebase/app");
+const firestore_1 = require("firebase/firestore");
+const firebaseApp = (0, app_1.initializeApp)({
+    apiKey: "AIzaSyD_LB7UtRh32wQ6mPZv6Wnp8q7EK-HIDYg",
+    authDomain: "thall-30b94.firebaseapp.com",
+    projectId: "thall-30b94",
+    storageBucket: "thall-30b94.appspot.com",
+    messagingSenderId: "549197004508",
+    appId: "1:549197004508:web:e14d810d640d9225e67e19",
+    measurementId: "G-ELKEZCLQVZ",
+});
+const firestore = (0, firestore_1.getFirestore)();
+//# sourceMappingURL=firestoreHandler.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,9 @@
 import express from "express";
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { cors } from "./middleware";
 import { postHandler } from "./handlers";
 
 const app = express();
 const port = 4000;
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyD_LB7UtRh32wQ6mPZv6Wnp8q7EK-HIDYg",
-  authDomain: "thall-30b94.firebaseapp.com",
-  projectId: "thall-30b94",
-  storageBucket: "thall-30b94.appspot.com",
-  messagingSenderId: "549197004508",
-  appId: "1:549197004508:web:e14d810d640d9225e67e19",
-  measurementId: "G-ELKEZCLQVZ",
-});
-
-const firestore = getFirestore();
 
 app.use(cors);
 
